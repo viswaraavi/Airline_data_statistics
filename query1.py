ccaccba2dfb4f2c3493868a75bbaee04e791904c
viswaraavi <viswaraavi@yahoo.in>
2016-11-23 13:58:26 -0500

    Querys update

--- query1.py
+++ query1.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import pandas as pd
+
 
 os.environ['SPARK_HOME'] = "/home/viswa/Downloads/spark-1.6.2-bin-hadoop2.6"
 sys.path.append("/home/viswa/Downloads/spark-1.6.2-bin-hadoop2.6/python")
@@ -20,10 +20,19 @@ sc = SparkContext(conf=conf)
 
 rdd=sc.textFile("/home/viswa/Music/2015.csv")
 mapped_rdd=rdd.map(lambda line:line.split(","))
+#USER can specify the airport id here
 filter_19790=mapped_rdd.filter(lambda list1:list1[7]==u'19790')
 filter_mapped=filter_19790.map(lambda list1:list1[31])
+def helper(x):
+    y=str(x)
+    z=y[1:len(x)-1]
+    return int(z)
+
+filter_mapped=filter_mapped.map(lambda x:helper(x))
 total_delay=filter_mapped.reduce(lambda a,b:a+b)
 print total_delay
+
+
 
 
 
