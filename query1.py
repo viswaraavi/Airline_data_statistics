6ae0d3ad6fdf26d02a08e972958b9b6ce85e96d6
Viswa Teja <viswaraavi@yahoo.in>
2016-11-24 17:58:04 -0500

    updated query1

--- query1.py
+++ query1.py
@@ -18,19 +18,24 @@ except ImportError as e:
 conf = SparkConf().setAppName("DIC FINAL PROJECT")
 sc = SparkContext(conf=conf)
 
-rdd=sc.textFile("s3n://csc591-dic-airline-data/0106.csv")
+rdd=sc.textFile("s3n://csc591-dic-airline-data/??1?.csv")
 mapped_rdd=rdd.map(lambda line:line.split(","))
-#USER can specify the airport id here
-filter_19790=mapped_rdd.filter(lambda list1:list1[7]==u'19790')
-filter_mapped=filter_19790.map(lambda list1:list1[31])
-def helper(x):
-    y=str(x)
-    z=y[1:len(x)-1]
-    return int(z)
+mapped_rdd.cache()
 
-filter_mapped=filter_mapped.map(lambda x:helper(x))
-total_delay=filter_mapped.reduce(lambda a,b:a+b)
-print total_delay
+def delay_airport(airport_id):
+    filter_19790=mapped_rdd.filter(lambda list1:list1[7]==airport_id)
+    def helper(x):
+        y=str(x)
+        z=y[1:len(x)-1]
+        return int(z)
+    filter_mapped=filter_19790.map(lambda list1:helper(list1[31]))
+    total_delay=filter_mapped.reduce(lambda a,b:a+b)
+    return total_delay
+
+print delay_airport(u'19790')
+print delay_airport(u'19789')
+delay_airport(u'19788')
+
 
 
 
