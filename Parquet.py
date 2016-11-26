9cb43e12264034a72b131797966e001c9db3fe83
simerdeep92 <sjolly@ncsu.edu>
2016-11-26 15:52:29 -0500

    Update Parquet.py

--- Parquet.py
+++ Parquet.py
@@ -19,15 +19,15 @@ def convert(sqlContext, filename, schema, tablename):
         .load(filename)
     df.write.parquet("s3n://csc591-dic-airline-data/"+tablename)
 
-convert(sqlContext,"s3n://csc591-dic-airline-data/??1?.csv","","dataset_5years")
+convert(sqlContext,"s3n://csc591-dic-airline-data/201?.csv","","dataset_5years")
 df = sqlContext.read.parquet("s3n://csc591-dic-airline-data/dataset_5years")
 df.registerTempTable("airline_tbl")
 
-df_final = df.select([c for c in df.columns if c in {'YEAR','QUARTER','MONTH','DAY_OF_MONTH','DAY_OF_WEEK','FL_DATE' ,'UNIQUE_CARRIER' ,'AIRLINE_ID' ,'CARRIER','FL_NUM','ORIGIN_AIRPORT_ID','ORIGIN','ORIGIN_CITY_NAME','ORIGIN_STATE_ABR','ORIGIN_STATE_NM','ORIGIN_WAC','DEST_AIRPORT_ID','DEST_AIRPORT_SEQ_ID','DEST_CITY_MARKET_ID','DEST','DEST_CITY_NAME','DEST_STATE_ABR','DEST_STATE_NM','DEST_WAC','CRS_DEP_TIME','DEP_TIME','DEP_DELAY_NEW','ARR_DELAY','ARR_DELAY_NEW','CANCELLED','CANCELLATION_CODE','ACTUAl_ELAPSED_TIME','AIR_TIME','FLIGHTS','DISTANCE','CARRIER_DELAY','WEATHER_DELAY','NAS_DELAY','SECURITY_DELAY','LATE_AIRCRAFT_DELAY'}])
+df_final = df.select([c for c in df.columns if c in {'YEAR','QUARTER','MONTH','DAY_OF_MONTH','DAY_OF_WEEK','FL_DATE' ,'UNIQUE_CARRIER' ,'AIRLINE_ID' ,'CARRIER','FL_NUM','ORIGIN_AIRPORT_ID','ORIGIN','ORIGIN_CITY_NAME','ORIGIN_STATE_ABR','ORIGIN_STATE_NM','ORIGIN_WAC','DEST_AIRPORT_ID','DEST_AIRPORT_SEQ_ID','DEST_CITY_MARKET_ID','DEST','DEST_CITY_NAME','DEST_STATE_ABR','DEST_STATE_NM','DEST_WAC','CRS_DEP_TIME','DEP_TIME','DEP_DELAY_NEW','ARR_DELAY_NEW','CANCELLED','CANCELLATION_CODE','ACTUAl_ELAPSED_TIME','AIR_TIME','FLIGHTS','DISTANCE','CARRIER_DELAY','WEATHER_DELAY','NAS_DELAY','SECURITY_DELAY','LATE_AIRCRAFT_DELAY'}])
 df_final.write.parquet("s3n://csc591-dic-airline-data/reduced_dataset");
 df = sqlContext.read.parquet("s3n://csc591-dic-airline-data/reduced_dataset")
 df.registerTempTable("airline_tbl")
-spark.sql("select * from airline_5 where month = 1 and weather_delay>1")
+#spark.sql("select * from airline_5 where month = 1 and weather_delay>1")
 
 
 def Delay_Statistics(start_year,end_year,start_month,end_month,airline_id=None,origin_city=None,dest_city=None):
@@ -39,11 +39,68 @@ def Delay_Statistics(start_year,end_year,start_month,end_month,airline_id=None,o
             WHERE FL_DATE >='"+start_date +"' AND FL_DATE<='" + end_date + "'\
             "
     if airline_id != None and airline_id !="":
-        sql_query = sql_query + "AND AIRLINE_ID = '" + airline_id + "'";
+        sql_query = sql_query + " AND AIRLINE_ID = '" + airline_id + "'";
     if origin_city !=None and origin_city !="":
-        sql_query = sql_query + "AND ORIGIN_CITY_NAME = '" + origin_city + "'";
+        sql_query = sql_query + " AND ORIGIN_CITY_NAME = '" + origin_city + "'";
     if dest_city !=None and dest_city !="":
-        sql_query = sql_query + "AND DEST_CITY_NAME = '" + dest_city + "'";
+        sql_query = sql_query + " AND DEST_CITY_NAME = '" + dest_city + "'";
     return spark.sql(sql_query)    
+
+def MostDelaysByCarrier(start_year,end_year,start_month,end_month,origin_city = None,dest_city = None):
+    start_date =  start_year + "-" + start_month + "-01"
+    end_date = end_year+"-" + end_month + "-31"
+    sql_query =  "SELECT CARRIER,AVG(ARR_DELAY_NEW) as ARR_DELAY_AVG , AVG(DEP_DELAY_NEW) AS DEP_DELAY_AVG \
+            FROM airline_tbl\
+            WHERE FL_DATE >='"+start_date +"' AND FL_DATE<='" + end_date + "'\
+          "
+    if origin_city !=None and origin_city !="":
+        sql_query = sql_query + " AND ORIGIN_CITY_NAME = '" + origin_city + "'";
+    if dest_city !=None and dest_city !="":
+        sql_query = sql_query + " AND DEST_CITY_NAME = '" + dest_city + "'";
+    sql_query = sql_query + "GROUP BY CARRIER"
+    intermediate_df = spark.sql(sql_query) 
+    intermediate_df.registerTempTable("temp_aggregate_tbl")    
+    sql_query = "SELECT CARRIER, ARR_DELAY_AVG,DEP_DELAY_AVG \
+                FROM temp_aggregate_tbl\
+                ORDER BY ARR_DELAY_AVG desc\
+                LIMIT 10";
         
+    return spark.sql(sql_query)    
+
+
+def CarrierWithMaximumCancelledFlights(start_year,end_year,start_month,end_month,origin_city = None,dest_city = None):
+    #returns carrier, cancelled_total, total
+    start_date =  start_year + "-" + start_month + "-01"
+    end_date = end_year+"-" + end_month + "-31"
+    sql_query =  "SELECT CARRIER, SUM(CANCELLED) as CANCELLED_TOTAL,COUNT(1) AS TOTAL \
+            FROM airline_tbl\
+            WHERE FL_DATE >='"+start_date +"' AND FL_DATE<='" + end_date + "'\
+          "
+    if origin_city !=None and origin_city !="":
+        sql_query = sql_query + " AND ORIGIN_CITY_NAME = '" + origin_city + "'";
+    if dest_city !=None and dest_city !="":
+        sql_query = sql_query + " AND DEST_CITY_NAME = '" + dest_city + "'";
+    sql_query = sql_query + "GROUP BY CARRIER"
+    intermediate_df = spark.sql(sql_query) 
+    intermediate_df.registerTempTable("temp_aggregate_tbl")    
+    sql_query = "SELECT CARRIER, CANCELLED_TOTAL,TOTAL \
+                FROM temp_aggregate_tbl\
+                ORDER BY CANCELLED_TOTAL desc\
+                LIMIT 10";   
+    
+    return spark.sql(sql_query)      
+
+def CarrierWithMaximumAirTime(start_year,end_year,start_month,end_month):
+    start_date =  start_year + "-" + start_month + "-01"
+    end_date = end_year+"-" + end_month + "-31"
+    sql_query =  "SELECT CARRIER, Air_Time, FL_NUM , ORIGIN_CITY_NAME ,DEST_CITY_NAME \
+            FROM airline_tbl\
+            WHERE FL_DATE >='"+start_date +"' AND FL_DATE<='" + end_date + "'"
+    sql_query = sql_query + "ORDER BY Air_Time desc limit 10"
+    return spark.sql(sql_query)
+
 Delay_Statistics("2011","2011","1","3")
+CarrierWithMaximumCancelledFlights("2011","2011","1","3")
+MostDelaysByCarrier("2011","2011","1","3")
+
+CarrierWithMaximumAirTime("2011","2011","1","3")
